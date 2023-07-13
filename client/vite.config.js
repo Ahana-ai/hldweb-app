8845afce7a774b9035729fd3ec0c0aed879bddd0
Ahana <ahanade100@gmail.com>
2023-07-13 16:38:14 +0530

    Adding build command 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,22 +9,23 @@ export default defineConfig({
         target:
           "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api1/, ''),
+        rewrite: (path) => path.replace(/^\/api1/, ""),
       },
       "/api2": {
-        target:
-          "http://localhost:3000",
+        target: 
+        // "http://localhost:3000",
+        "https://hldweb-app-transactional-log.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api2/, ''),
+        rewrite: (path) => path.replace(/^\/api2/, ""),
       },
       "/api3": {
         target:
           // "https://crudcrud.com/api/de9809e23e8246e8a63538669bb8efdf/ericssondemohlr/",
-          "http://localhost:5000",
+          // "http://localhost:5000",
+          "https://hldweb-app-crud-service.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api3/, ''),
+        rewrite: (path) => path.replace(/^\/api3/, ""),
       },
-  
     },
   },
   plugins: [react()],
