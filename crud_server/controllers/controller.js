b0f12377ed30799d089e00ee272d278ab18105b9
Ahana <ahanade100@gmail.com>
2023-07-31 18:51:46 +0530

    Setting up for Docker & creating get api with Msisdn

--- crud_server/controllers/controller.js
+++ crud_server/controllers/controller.js
@@ -13,7 +13,7 @@ class DataController {
         msisdn: payload.msisdn,
       });
 
-      if (isUserExists) {
+      if (isUserExists && !isUserExists.length == 0) {
         return res.status(400).json({ message: "Data Already Exists" });
       } else {
         const data = {
@@ -86,6 +86,39 @@ class DataController {
         isDeleted: false,
       });
 
+      // const data = {
+      //   GetResponseSubscriber: {
+      //     imsi: payload.imsi,
+      //     msisdn: payload.msisdn,
+      //     hlrsn: payload.hlrsn,
+      //     cardType: payload.cardType,
+      //     nam: payload.nam,
+      //     services: {
+      //       clip: {
+      //         prov: payload.services.clip.prov,
+      //       },
+      //       smsmt: payload.services.smsmt,
+      //       optgprss: {
+      //         optgprs: payload.services.optgprss.optgprs,
+      //       },
+      //       odboc: {
+      //         odboc: payload.services.odboc.odboc,
+      //       },
+      //       odbroam: {
+      //         odbroam: payload.services.odbroam.odbroam,
+      //       },
+      //       category: {
+      //         category: payload.services.category.category,
+      //       },
+      //       eps: {
+      //         prov: payload.services.eps.prov,
+      //       },
+      //       smdp: payload.services.smdp,
+      //     },
+      //     rroption: payload.rroption,
+      //     skey: payload.skey,
+      //   },
+      // };
       if (!data) {
         return res.status(404).json({ error: "Data not found" });
       }
@@ -93,6 +126,29 @@ class DataController {
       return res.status(200).json(data);
     } catch (error) {
       console.log("Error while getDataByImsi API call: ", error.message);
+      return res.status(500).json(error.message);
+    }
+  }
+
+  /**
+   * @method getDataByMsisdn
+   */
+  async getDataByMsisdn(req, res) {
+    try {
+      const { msisdn } = req.params;
+
+      const data = await Data.findOne({
+        "GetResponseSubscriber.msisdn": msisdn,
+        isDeleted: false,
+      });
+
+      if (!data) {
+        return res.status(404).json({ error: "Data not found" });
+      }
+
+      return res.status(200).json(data);
+    } catch (error) {
+      console.log("Error while getDataByMsisdn API call: ", error.message);
       return res.status(500).json(error.message);
     }
   }
--- crud_server/database/db.js
+++ crud_server/database/db.js
@@ -3,9 +3,16 @@ import mongoose from "mongoose";
 const Connections = async () => {
   const USER = process.env.DB_USERNAME;
   const PASSWORD = process.env.DB_PASSWORD;
+  const MONGO_PORT = process.env.MONGO_PORT;
+  const SRVVAL = process.env.SRVVAL;
+  const MONGOCONT_NAME = process.env.MONGOCONT_NAME;
 
-  const URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.djmj1ek.mongodb.net/Hld-web-app`;
-
+  // const URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.djmj1ek.mongodb.net/Hld-web-app`;
+  if (MONGO_PORT) {
+    URL = `mongodb+${SRVVAL}://${USER}:${PASSWORD}@${MONGOCONT_NAME}:${MONGO_PORT}/Hld-web-app`;
+  } else {
+    URL = `mongodb+${SRVVAL}://${USER}:${PASSWORD}@${MONGOCONT_NAME}/Hld-web-app`;
+  }
   try {
     await mongoose.connect(URL, {
       useUnifiedTopology: true,
